19fb75cf7becc7909e866539bdba39268c8d3e23
akshitwakodikar <128572144+akshitwakodikar@users.noreply.github.com>
2025-05-21 13:29:29 +0530

    Update server.js backticks adjusted

--- Backend/server.js
+++ Backend/server.js
@@ -46,5 +46,5 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(port, () => {
-  console.log(Server is running on http://localhost:${port});
+  console.log(`Server is running on http://localhost:${port}`);
 });
